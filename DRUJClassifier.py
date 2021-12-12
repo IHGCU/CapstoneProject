80d85f56fe52469ec82d0444e493e44b1f47965f
Laurie Wells <81059294+IHGCU@users.noreply.github.com>
2021-12-12 10:10:32 -0700

    Add files via upload

--- DRUJClassifier.py
+++ DRUJClassifier.py
@@ -5,13 +5,14 @@ Created on Tue Nov 30 09:40:03 2021
 @author: AnLWells
 """
 
-# Load required packages and functions
+# Load required packages
 
-import numpy as np
+import PySimpleGUI as sg
 import matplotlib.pyplot as plt
 from keras.models import model_from_json
+import numpy as np
 
-# Define function for reading DICOM Images
+# Define a function for reading DICOM Images
 
 import pydicom
 from pydicom.pixel_data_handlers.util import apply_voi_lut
@@ -38,23 +39,35 @@ def read_xray(path, voi_lut = True, fix_monochrome = True):
 
 # Filenames for DICOM images
 
-filename01 = 'TestCases\Subject001\PAView'
-filename02 = 'TestCases\Subject001\ObliqueView'
-filename03 = 'TestCases\Subject001\LateralView'
+filename01 = sg.popup_get_file('Enter the file for the PA View')
+filename02 = sg.popup_get_file('Enter the file for the Oblique View')
+filename03 = sg.popup_get_file('Enter the file for the Lateral View')
 
 # Load and look at the images
 
 img1 = read_xray(filename01)
 plt.figure(figsize = (12,12))
 plt.imshow(img1, 'gray')
+plt.title("PA View")
+plt.show
+
 
 img2 = read_xray(filename02)
 plt.figure(figsize = (12,12))
 plt.imshow(img2, 'gray')
+plt.title("Oblique Veiw")
+plt.show
+
 
 img3 = read_xray(filename03)
 plt.figure(figsize = (12,12))
 plt.imshow(img3, 'gray')
+plt.title("Lateral View")
+plt.show
+
+
+plt.figure(figsize = (12,12))
+plt.show
 
 # Crop images
 
@@ -75,11 +88,6 @@ img3cr = img3[np.round(x3[0]/2).astype(int)-500:np.round(x3[0]/2).astype(int)+50
 ComboView = np.append(img1cr, img2cr, axis=1)
 ComboView = np.append(ComboView, img3cr, axis=1)
 
-# View Combined view
-
-plt.imshow(ComboView, cmap='gray')  
-plt.show
-
 # Reduce resolution of combined view
 
 ComboReduce = np.zeros((500, 1050))
@@ -89,7 +97,13 @@ for k in range(500):
     for i in range(1050):
         ComboReduce[k,i] = round((ComboViewN[2*k,2*i]+ComboViewN[2*k+1,2*i]+
                                     ComboViewN[2*k, 2*i+1]+ComboViewN[2*k+1,2*i+1])/4)           
-        
+# View Combined view 
+
+plt.figure(figsize=(6, 3))
+plt.imshow(ComboView, cmap='gray') 
+plt.title("Classified fracture, Combined View") 
+plt.savefig('C:/Users/AnLWells/Documents/CapstoneProject/Classifier/myFigure.png')
+
 # Reshape the input to match the input to the model
 
 ComboReduce = np.reshape(ComboReduce,(1,500,1050,1))
@@ -97,29 +111,54 @@ ComboReduce = np.reshape(ComboReduce,(1,500,1050,1))
 # Load and run the model
 
 # load json and create model
-json_file = open('model.json', 'r')
+json_file = open('C:/Users/AnLWells/Documents/CapstoneProject/Classifier/model.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 loaded_model = model_from_json(loaded_model_json)
 # load weights into new model
-loaded_model.load_weights("model.h5")
+loaded_model.load_weights("C:/Users/AnLWells/Documents/CapstoneProject/Classifier/model.h5")
+print(" ")
 print("Loaded model from disk")
 # evaluate loaded model on test data
 loaded_model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 # score = loaded_model.evaluate(X, Y, verbose=0)
 # print("%s: %.2f%%" % (loaded_model.metrics_names[1], score[1]*100))
 
-# Use the model to predict classification of fracture
+# Use the model to predict classification of fracture and produce the output for the model
 
 predict_x=loaded_model.predict(ComboReduce)
 classes_x=np.argmax(predict_x,axis=1)
+
 if classes_x==0:
-    print("Type of fracture is I")
+    str01=" Type of fracture is I"
+    str05=" A typical type I fracture is shown with the classified fracture below"
+    filename04 = 'C:/Users/AnLWells/Documents/CapstoneProject/Classifier/TypeI.png'
 else:
     if classes_x==1:
-        print("Type of fracture is II")
+        str01=" Type of fracture is II"
+        str05=" A typical type II fracture is shown with the classified fracture below"
+        filename04 = 'C:/Users/AnLWells/Documents/CapstoneProject/Classifier/TypeII.png'
     else:
-        print("Type of fracture is III")
-print("Type I:   ", round(predict_x[0,0]*100,2))
-print("Type II:  ", round(predict_x[0,1]*100,2))
-print("Type III: ", round(predict_x[0,2]*100,2))
+        str01=" Type of fracture is III"
+        str05=" A typical type III fracture is shown with the classified fracture below"
+        filename04 = 'C:/Users/AnLWells/Documents/CapstoneProject/Classifier/TypeIII.png'
+
+str02=' Type I:   ' + str(round(predict_x[0,0]*100,2))
+str03=' Type II:  ' + str(round(predict_x[0,1]*100,2))
+str04=' Type III: ' + str(round(predict_x[0,2]*100,2))
+
+layout10 = [[sg.Listbox(values=[' Report for Classification of DRUJ Fracture', 
+                                ' ', str01, ' ', 
+                                ' The softmax numbers for each type of fracture are: ',
+                                ' ', str02, str03, str04,
+                                ' ', ' ', str05], size=(60,15), font=('Times', 14))],             
+            [sg.Image(filename04)],                        
+            [sg.Image('C:/Users/AnLWells/Documents/CapstoneProject/Classifier/myFigure.png')],
+            [sg.Button('Ok')]]
+
+window = sg.Window('Model Output',layout10)
+
+event, values = window.read()
+
+window.close()
+
